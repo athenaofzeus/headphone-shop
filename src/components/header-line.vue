5af02c98ab5763e36632e675a57b1049aa65a9b9
Athena <valieva.afina@mail.ru>
2022-08-14 21:41:43 +0300

    Upload all files

--- src/components/header-line.vue
+++ src/components/header-line.vue
@@ -1,15 +1,46 @@
 <template>
     <header class="work-area__header work-area__header_padding work-area__header_mrgn">
-        <img :src="require('../../public/img/top_icons/logo.svg')" alt="logo" class="work-area__main-logo">
-
+        <a href="#">
+            <img :src="require('../../public/img/top_icons/logo.svg')" alt="logo" class="work-area__main-logo">
+        </a>
         <div class="work-area__grope-logos">
-            <span class="work-area__logo-ellips" v-for="elem in iconNames" :key="elem">
-                <img :src="require(`../../public/img/top_icons/user_icon/${elem}.svg`)" alt="loupe" class="work-area__loupe-logo">
-                <img :src="require('../../public/img/top_icons/user_icon/Ellipse.svg')" alt="ellips" class="work-area__ellips">
-            </span>
-        </div>
+            <input class="work-area__search-input" 
+            :placeholder="placeHoldStr"
+            v-model="userSearch"
+            @keypress.enter="startSearch" 
+            type="text" 
+            v-show="searchPanelvisib">
 
-        <img :src="require('../../public/img/top_icons/hamb_menu.svg')" alt="hamb_menu" class="work-area__hamb-menu">
+            <span class="work-area__logo-ellips" 
+                v-for="elem in iconNames" 
+                :key="elem">
+                <img :src="require(`../../public/img/top_icons/user_icon/${elem}.svg`)" 
+                alt="loupe" 
+                class="work-area__loupe-logo" 
+                @click="openSearch(elem)" 
+                v-if="elem != 'loupe' ? otherLogosVisib : true">
+                <img :src="require('../../public/img/top_icons/user_icon/Ellipse.svg')" 
+                alt="ellips" 
+                class="work-area__ellips" 
+                v-if="elem != 'loupe' ? otherLogosVisib : true">
+            </span>
+
+        </div>
+        <div class="work-area__parent-menu">
+            <img :src="require('../../public/img/top_icons/menu.png')" 
+                alt="hamb_menu" 
+                class="work-area__hamb-menu">
+                <div class="work-area__menu-cover"
+                    @mouseenter="callMenu"
+                    @click="callMenu">
+                </div>
+        </div>
+        <div class="hiddenMenu"
+            v-if="menuVisib">
+            <ul @mouseleave="leaveMenu">
+                <li v-for="item in menuList" :key="item">{{ item }}</li>
+            </ul>
+        </div>
     </header>
 </template>
 
@@ -21,6 +52,31 @@ export default {
     return {
         path : '../../public/img/top_icons/',
         iconNames : ['loupe', 'store', 'account'],
+        searchPanelvisib: false,
+        otherLogosVisib: true,
+        menuVisib : false,
+        placeHoldStr : 'Press Enter to search',
+        userSearch : '',
+        menuList: ['Basket', 'New Collection']
+    }
+  },
+  methods : {
+    openSearch (element) {
+        if (element == 'loupe') {
+            this.searchPanelvisib = !this.searchPanelvisib;
+            this.otherLogosVisib = !this.otherLogosVisib;
+        }
+    },
+    startSearch(event) {
+        //добавить поиск продуктов
+        console.log(event.key)
+
+    },
+    callMenu() {
+        this.menuVisib = !this.menuVisib;
+    },
+    leaveMenu () {
+        this.menuVisib = false;
     }
   }
 };
